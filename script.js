26119aed9f344ddb4abba18a2ee46b4f6fb5dcdf
sabrinebizbiz28-droid <sabrine.bizbiz.28@edu.uiz.ac.ma>
2026-04-23 23:51:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ console.log("La différence est: " + (num1 - num2));
 console.log("Le produit est: " + (num1 * num2));
 console.log("Le quotient est: " + (num1 / num2));
 */
+/*
 let nombreADeviner = Math.floor(Math.random() * 10) + 1;
 let tentative;
 let score = 0;
@@ -20,3 +21,21 @@ while (tentative != nombreADeviner) {
 }
 alert("Bravo ! Vous avez trouvé le nombre en " + score + " tentatives.");
 console.log("Score final : " + score);
+*/
+const questions = [
+    { q: "Quelle est la capitale du Maroc ?", a: "Rabat" },
+    { q: "Combien font 5 + 5 ?", a: "10" },
+    { q: "Quelle est la couleur du ciel ?", a: "Bleu" }
+];
+let scoreQuiz = 0;
+for (let i = 0; i < questions.length; i++) {
+    let reponse = prompt(questions[i].q)
+    if (reponse && reponse.toLowerCase() === questions[i].a.toLowerCase()) {
+        alert("Bonne réponse !");
+        scoreQuiz++;
+    } else {
+        alert("Mauvaise réponse. La réponse était : " + questions[i].a);
+    }
+}
+alert("Quiz terminé ! Votre score est : " + scoreQuiz + "/" + questions.length);
+console.log("Score final Quiz : " + scoreQuiz + "/3");
