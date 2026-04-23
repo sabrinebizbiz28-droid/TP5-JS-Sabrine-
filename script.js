66ef29b45b1a5bda419e960bfc52242827150811
sabrinebizbiz28-droid <sabrine.bizbiz.28@edu.uiz.ac.ma>
2026-04-23 21:42:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let num1 = prompt ("Saisir le premier nombre:");
+let num2 = prompt ("Saisir le deuxiém nombre");
+num1 = parseFloat( num1);
+num2 = parseFloat(num2);
+console.log("La somme est:"+(num1+num2));
+console.log ("La différence est:"+(num1-num2))
+console.log ("La différence est:"+(num1*num2))
+console.log ("La différence est:"+(num1/num2))
