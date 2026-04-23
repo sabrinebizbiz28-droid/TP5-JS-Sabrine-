5dda0e9b0d25ceb7bf65db972c9a55124d172274
sabrinebizbiz28-droid <sabrine.bizbiz.28@edu.uiz.ac.ma>
2026-04-23 23:22:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let num1 = prompt ("Saisir le premier nombre:");
-let num2 = prompt ("Saisir le deuxiém nombre");
-num1 = parseFloat( num1);
-num2 = parseFloat(num2);
-console.log("La somme est:"+(num1+num2));
-console.log ("La différence est:"+(num1-num2))
-console.log ("La différence est:"+(num1*num2))
-console.log ("La différence est:"+(num1/num2))
+/* 
+let num1 = parseFloat(prompt("Saisir le premier nombre:"));
+let num2 = parseFloat(prompt("Saisir le deuxième nombre:"));
+console.log("La somme est: " + (num1 + num2));
+console.log("La différence est: " + (num1 - num2));
+console.log("Le produit est: " + (num1 * num2));
+console.log("Le quotient est: " + (num1 / num2));
+*/
+let nombreADeviner = Math.floor(Math.random() * 10) + 1;
+let tentative;
+let score = 0;
+while (tentative != nombreADeviner) {
+    tentative = prompt("Devinez le nombre entre 1 et 10 :");
+    score++;
+    if (tentative > nombreADeviner) {
+        alert("Plus petit !");
+    } else if (tentative < nombreADeviner) {
+        alert("Plus grand !");
+    }
+}
+alert("Bravo ! Vous avez trouvé le nombre en " + score + " tentatives.");
+console.log("Score final : " + score);
